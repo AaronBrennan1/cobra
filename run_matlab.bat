529facbd75ba83ba6ba5a4714d297e6e3dcf7a8a
AaronBrennan1 <aaronbrennan.brennan@gmail.com>
2023-05-02 15:59:50 +0100

    t

--- run_matlab.bat
+++ run_matlab.bat
@@ -1,3 +1,6 @@
 @echo off
-"C:\Program Files\MATLAB\R2022b\bin\matlab.exe" -batch "addpath('.'); convert_selected_files('%*')"
+echo Current working directory: %cd%
+echo Location of convert_files.m: %cd%\convert_files.m
+"C:\Program Files\MATLAB\R2022b\bin\matlab.exe" -batch "addpath('.'); convert_files"
+
 
